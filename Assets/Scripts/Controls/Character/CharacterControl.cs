c10dccd869f241677276dea2027b16e12b4be2a9
whycallculate <whycallculate@gmail.com>
2024-11-06 14:17:30 +0300

    Gameplay And UI added

--- Assets/Scripts/Controls/Character/CharacterControl.cs
+++ Assets/Scripts/Controls/Character/CharacterControl.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 public class CharacterControl : CalculateCharacterValues
 {
     private UIManager uiManager;
-    private const int healt = 100;
+    private const int health = 100;
     private const int shield = 100;
     private const int energy = 5;
     private const int power = 5;
-    private int currentHealt;
+    private int currentHealth;
     private int shieldCurrent;
     private int energyCurrent;
     private int powerCurrent;
     
     private void Awake() 
     {
-        currentHealt = healt;   
+        currentHealth = health;   
         energyCurrent = energy;
         uiManager = UIManager.Instance; 
         
