34cd413d8580e2ae05650e476316e4bd91f3043c
Berkay Kurt <kurtberkay944@gmail.com>
2025-04-14 23:09:03 +0300

    Make deck upgrade.

--- Assets/Scripts/Deck/DeckUIManager.cs
+++ Assets/Scripts/Deck/DeckUIManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using CardObjectCommon_Features;
@@ -13,15 +14,16 @@ namespace Deck
         [SerializeField] private Transform deckItenPositionContent;
         [SerializeField] private Transform deckItemScrollRectContent;
         [SerializeField] private Button DeckBackReturnButton;
-        [SerializeField] private Queue<DeckItemPosition> itemPositions = new Queue<DeckItemPosition>();
-        public Queue<DeckItemPosition> ItemPositions => itemPositions;
+        [SerializeField] private List<DeckItemPosition> itemPositions = new List<DeckItemPosition>();
+        public List<DeckItemPosition> ItemPositions => itemPositions;
+        private int index = 0;
 
         public void DeckItemPositionAbjust(int deckItemPositionCount)
         {
             for (int i = 0; i < deckItemPositionCount; i++)
             {
                 DeckItemPosition newItemPosition = Instantiate(deckItemPositionPrefab,deckItenPositionContent).GetComponent<DeckItemPosition>();
-                itemPositions.Enqueue(newItemPosition);
+                itemPositions.Add(newItemPosition);
                 newItemPosition.name = newItemPosition.name.Replace("_Prefab(Clone)","");
                 newItemPosition.name = newItemPosition.name + $" {i}";
             }
@@ -43,6 +45,7 @@ namespace Deck
                 item.GetComponent<CardMovement>().enabled = false;
                 item.SetActive(true);
                 item.transform.SetParent(deckItemScrollRectContent);
+
                 Button itemButton = item.GetComponent<Button>();
                 if(itemButton.onClick == null)
                 {
@@ -54,45 +57,87 @@ namespace Deck
                 {
                     if(itemPositions.Count > 0)
                     {
-                        CardObjectCommonFeatures card = GameManager.Instance.FindCardType(itemButton.gameObject);
+                        CardObjectCommonFeatures card = FindItemScriptType(itemButton.gameObject);
                         
-                        DeckItemPosition deckItemPosition = DequeueDeckItemPosition();
-                        card.DeckPosition = deckItemPosition;
-                        print(card.DeckPosition);
+                        var tuple = DequeueDeckItemPosition();
+                        DeckItemPosition deckItemPosition = tuple.Item2;
 
-                        deckItemPosition.SetDeckItemPosition();
-                        RectTransform itemReckTransform = itemButton.GetComponent<RectTransform>();
-                        itemReckTransform.sizeDelta = deckItemPosition.GetDeckItemPositionSizeDelta();
-                        item.transform.SetParent(deckItemPosition.transform);
-                        item.transform.position = deckItemPosition.transform.position;
+                        if(tuple.Item1)
+                        {
+                            card.DeckPosition = deckItemPosition;
+                            
+                            deckItemPosition.SetDeckItemPosition();
+                            RectTransform itemReckTransform = itemButton.GetComponent<RectTransform>();
+                            itemReckTransform.sizeDelta = deckItemPosition.GetDeckItemPositionSizeDelta();
+                            item.transform.SetParent(deckItemPosition.transform);
+                            item.transform.position = deckItemPosition.transform.position;
+                            OutItemButtonFromDeck(itemButton);
 
-                        OutItemButtonFromDeck(itemButton,itemReckTransform,card.DeckPosition);
+                        }
                     }
                 });
             }
         }
-
-
-
-        private void OutItemButtonFromDeck(Button itemButton,RectTransform itemReckTransform,DeckItemPosition deckPosition)
+        
+        private CardObjectCommonFeatures FindItemScriptType(GameObject item)
         {
-            ItemButtonReset(itemButton);
-            itemButton.onClick.AddListener(delegate
-            {
-                itemButton.transform.SetParent(deckItemScrollRectContent);
-                AddItemToDeck(itemButton,itemReckTransform,deckPosition);
-            });
+            return GameManager.Instance.FindCardType(item);
         }
 
-        private void AddItemToDeck(Button itemButton,RectTransform itemReckTransform,DeckItemPosition deckItemPosition)
+
+        private void OutItemButtonFromDeck(Button itemButton)
+        {
+            if (itemButton.onClick == null)
+            {
+                itemButton.onClick = new Button.ButtonClickedEvent();
+            }
+
+            itemButton.onClick.RemoveAllListeners();
+            itemButton.onClick.AddListener(()=>
+            {
+                print(index);
+                itemButton.transform.SetParent(deckItemScrollRectContent);
+                if(index > 0)
+                {
+                    index--;
+                }
+                AddItemToDeck(itemButton);
+            });
+
+        }
+
+        private void AddItemToDeck(Button itemButton)
         {
             ItemButtonReset(itemButton);
             itemButton.onClick.AddListener(()=>
             {
-                itemButton.transform.SetParent(deckItemPosition.transform);
-                itemButton.transform.position = deckItemPosition.transform.position;
-                itemReckTransform.sizeDelta =deckItemPosition.GetDeckItemPositionSizeDelta();
+                var tuple = DequeueDeckItemPosition();
+                DeckItemPosition deckItemPosition = tuple.Item2;
+
+                if (tuple.Item1)
+                {
+
+                    deckItemPosition.SetDeckItemPosition();
+                    RectTransform itemReckTransform = itemButton.GetComponent<RectTransform>();
+                    itemReckTransform.sizeDelta = deckItemPosition.GetDeckItemPositionSizeDelta();
+                    itemButton.transform.SetParent(deckItemPosition.transform);
+                    itemButton.transform.position = deckItemPosition.transform.position;
+                    
+                   
+                    OutItemButtonFromDeck(itemButton);
+                }
             });
+            
+            /*
+            ItemButtonReset(itemButton);
+            var tuple = DequeueDeckItemPosition();
+            DeckItemPosition itemPosition = tuple.Item2;
+            itemButton.transform.SetParent( itemPosition.transform);
+            itemButton.transform.position = itemPosition.transform.position;
+            print(itemPosition + "-" + index);
+            print(itemPositions[index].name);
+            index++;
+            */
         }
 
         private void ItemButtonReset(Button itemButton)
@@ -126,19 +171,32 @@ namespace Deck
         }
 
 
-        public DeckItemPosition DequeueDeckItemPosition()
+        public Tuple<bool,DeckItemPosition> DequeueDeckItemPosition()
         {
+            /*
             if(itemPositions.Count > 0)
             {
                 DeckItemPosition deckItemPosition = itemPositions.Dequeue();
-                print(deckItemPosition.name);
                 return deckItemPosition;
             }
-            return null;
+            */
+            if(index < itemPositions.Count)
+            {
+                DeckItemPosition deckItemPosition = itemPositions[index];
+                index++;
+                print(index);
+                return Tuple.Create(true,deckItemPosition);
+            }
+            else
+            {
+                DeckItemPosition deckItemPosition = null;
+                return Tuple.Create(false,deckItemPosition);
+            }
         }
 
         public void SetDeckItenPositionFull()
         {
+            /*
             if(itemPositions.Dequeue() != null)
             {
                 DeckItemPosition deckItemPosition = itemPositions.Dequeue();
@@ -157,15 +215,18 @@ namespace Deck
                 }
 
             }
+            */
         }
 
         public void RemoveDeckItenPositionFull()
         {
+            /*
             for (int i = 0; i < itemPositions.Count; i++)
             {
                 DeckItemPosition deckItemPosition = itemPositions.Dequeue();
                 deckItemPosition.RemoveDeckItemPosition();
             }
+            */
         }
 
        
